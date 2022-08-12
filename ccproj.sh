325a05f77564aaed6654e34ee7b8fa6bfdaca1dd
teguhsatyadharma <hiteguhsatya@gmail.com>
2022-08-12 10:37:56 +0700

    Init

---
+++ ccproj.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Create CPP Project using your CLI."
+   echo
+   echo "Syntax: scriptTemplate [-n|h|v|V]"
+   echo "options:"
+   echo "n     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+
